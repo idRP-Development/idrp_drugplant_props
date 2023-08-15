b8bf8ef646f26c8188d8c7828b36c06a29f56061
idRP <88186466+idrp@users.noreply.github.com>
2023-08-15 10:05:25 +0930

    Add files via upload

---
+++ idrp_drugplant_props/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+version '1.0'
+
+description 'Drug Planting Props'
+
+author 'idART'
+
+files{
+	'stream/*.*'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
+
+lua54 'yes'
